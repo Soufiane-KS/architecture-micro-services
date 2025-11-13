1dc8fe2c7edb5ac3ef4b21a78d729a0c31fc7a42
Soufiane-KS <soufiane.kousta@uit.ac.ma>
2025-11-13 20:45:32 +0100

    added configuration service and tested customer service configuration

--- customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
@@ -1,15 +1,18 @@
 package com.example.customerservice;
 
+import com.example.customerservice.config.CustomerConfigParams;
 import com.example.customerservice.entities.Customer;
 import com.example.customerservice.repository.CustomerRepository;
 import org.apache.hc.core5.reactor.Command;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
